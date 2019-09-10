6b03e37416ac8b77dcce7b615af47f463d8ea9c8
zetaPRIME <qmanxt@gmail.com>
2019-09-10 01:25:00 -0400

    consistency

--- multibound2/data/config.cpp
+++ multibound2/data/config.cpp
@@ -1,5 +1,7 @@
 #include "config.h"
 
+#include "util.h"
+
 #include <QDir>
 #include <QStandardPaths>
 #include <QDebug>
@@ -13,18 +15,18 @@ QString MultiBound::Config::starboundRoot;
 void MultiBound::Config::load() {
     configPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation); // already has "multibound" attached
     if (auto d = QDir(configPath); !d.exists()) d.mkpath(".");
-    instanceRoot = QDir::cleanPath(configPath + "/instances/");
+    instanceRoot = Util::splicePath(configPath, "/instances/");
     if (auto d = QDir(instanceRoot); !d.exists()) d.mkpath(".");
 
     QDir home(QStandardPaths::writableLocation(QStandardPaths::HomeLocation));
 
-    starboundPath = QDir::cleanPath(home.absolutePath() + "/.steam/steam/steamapps/common/Starbound/linux/run-client.sh");
+    starboundPath = Util::splicePath(home, "/.steam/steam/steamapps/common/Starbound/linux/run-client.sh");
     QDir r(starboundPath);
     while (r.dirName().toLower() != "starbound") {r.cdUp(); if (r.isRoot()) break;}
     starboundRoot = QDir::cleanPath(r.absolutePath());
 
     // if (starboundPath.contains("/steamapps/")) { }
-    workshopRoot = QDir::cleanPath(home.absolutePath() + "/.steam/steam/steamapps/workshop/content/211820/");
+    workshopRoot = Util::splicePath(home, "/.steam/steam/steamapps/workshop/content/211820/");
 
     //
 }
--- multibound2/util.h
+++ multibound2/util.h
@@ -11,6 +11,7 @@ namespace MultiBound {
     namespace Util {
 
         inline QString splicePath(const QString& p1, const QString& p2) { return QDir::cleanPath(qs("%1/%2").arg(p1).arg(p2)); }
+        inline QString splicePath(const QDir& d, const QString& p) { return splicePath(d.absolutePath(), p); }
 
         QJsonDocument loadJson(const QString& path);
     }
